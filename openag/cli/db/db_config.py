ab979bbdbd85e14d63520309cf7974204af77e0d
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-02-17 00:21:57 +0000

    Serve /var/www/html from _app

--- openag/cli/db/db_config.py
+++ openag/cli/db/db_config.py
@@ -13,8 +13,10 @@ def generate_config(api_url=None):
             "reduce_limit": "false"
         }
     }
+    app_path = "/var/www/html"
     if api_url:
         config["httpd_global_handlers"] = {
-            "_openag": "{{couch_httpd_proxy, handle_proxy_req, <<\"{}\">>}}".format(api_url)
+            "_openag": "{{couch_httpd_proxy, handle_proxy_req, <<\"{}\">>}}".format(api_url),
+            "_app": "{{couch_httpd_misc_handlers, handle_utils_dir_req, \"{}\"}}".format(app_path)
         }
     return config
